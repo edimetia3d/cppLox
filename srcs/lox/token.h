fa03d7448dc2d2cc71c9f7558bee1c2c65580e80
edimetia3d <edimetia3d@gmail.com>
2021-04-04 13:59:01 +0000

    ref: abstract token type header

--- srcs/lox/token.h
+++ srcs/lox/token.h
@@ -7,30 +7,9 @@
 
 #include <string>
 
+#include "lox/token_type.h"
+
 namespace lox {
-
-// clang-format off
-enum class TokenType {
-  // Single-character tokens.
-  LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
-  COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
-
-  // One or two character tokens.
-  BANG, BANG_EQUAL,
-  EQUAL, EQUAL_EQUAL,
-  GREATER, GREATER_EQUAL,
-  LESS, LESS_EQUAL,
-
-  // Literals.
-  IDENTIFIER, STRING, NUMBER,
-
-  // Keywords.
-  AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
-  PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
-
-  EOF_TOKEN
-};
-// clang-format on
 
 class Token {
  public:
---
+++ srcs/lox/token_type.h
@@ -0,0 +1,31 @@
+//
+// License: MIT
+//
+
+#ifndef CPPLOX_SRCS_LOX_TOKEN_TYPE_H_
+#define CPPLOX_SRCS_LOX_TOKEN_TYPE_H_
+namespace lox {
+// clang-format off
+enum class TokenType {
+  // Single-character tokens.
+  LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+  COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+  // One or two character tokens.
+  BANG, BANG_EQUAL,
+  EQUAL, EQUAL_EQUAL,
+  GREATER, GREATER_EQUAL,
+  LESS, LESS_EQUAL,
+
+  // Literals.
+  IDENTIFIER, STRING, NUMBER,
+
+  // Keywords.
+  AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+  PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+  EOF_TOKEN
+};
+// clang-format on
+}  // namespace lox
+#endif  // CPPLOX_SRCS_LOX_TOKEN_TYPE_H_
