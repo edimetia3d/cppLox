06c9bc1a323dba062b047b112de915957e774627
edimetia3d <edimetia3d@gmail.com>
2021-07-19 00:27:11 +0800

    add: simple ast node generator

---
+++ srcs/lox/ast_nodes/expr.h
@@ -0,0 +1,26 @@
+//
+// License: MIT
+//
+//
+
+#ifndef CPPLOX_EXPR_H
+#define CPPLOX_EXPR_H
+
+#include "lox/token.h"
+
+namespace lox {
+template <class RetT>
+class Visitor {};
+/**
+ * An abstract class
+ */
+class Expr {
+  template <class RetT>
+  RetT accept(const Visitor<RetT>& v);
+};
+
+}  // namespace lox
+
+#include "lox/ast_nodes/expr_decl.h.inc"
+
+#endif  // CPPLOX_EXPR_H
---
+++ srcs/lox/ast_nodes/nodes_code_writer.py
@@ -0,0 +1,69 @@
+import sys
+from os import path
+
+all_def = {
+    "Binary": "Expr left, Token op, Expr right",
+    "Grouping": "Expr expression",
+    "Literal": "void* value",
+    "Unary": "Token op, Expr right"
+}
+
+file_template = """
+namespace lox{{
+class Expr;
+class Token;
+template<class RetT>
+class Visitor;
+
+{class_decls}
+
+}}
+"""
+
+class_template = """
+class {class_name}:public Expr
+{{
+public:
+{class_name}({init_params})
+:{init}{{}}
+{member_def}
+
+template <class RetT>
+RetT Accept(const Visitor<RetT>& visitor){{
+  return visitor.Visit{class_name}(this);
+}}
+  
+}};
+"""
+
+
+def gen_code(output_file_path):
+    with open(output_file_path, "w") as output_file:
+        class_decls = ""
+        for class_name in all_def:
+            member_list = all_def[class_name].split(",")
+            member_def = ""
+            member_init_params = []
+            member_init = []
+            for member in member_list:
+                cut_by_space = list(filter(lambda x: x != "", member.split(" ")))
+                member_type = cut_by_space[0]
+                member_name = cut_by_space[1]
+                member_def = member_def + f"{member_type}& {member_name};\n"
+                member_init_params.append(f"{member_type}& {member_name}")
+                member_init.append(f"{member_name}({member_name})")
+            member_init = ",\n".join(member_init)
+            member_init_params = ",".join(member_init_params)
+            class_decls = class_decls + class_template.format(class_name=class_name,
+                                                              init_params=member_init_params,
+                                                              init=member_init,
+                                                              member_def=member_def)
+        output_file.write(file_template.format(class_decls=class_decls))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        output_file_path = sys.argv[1]
+    else:
+        output_file_path = "tmp_gen_output.h.inc"
+    gen_code(output_file_path)
