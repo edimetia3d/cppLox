7954c9fde39f51b4c32fe61f2fe6f7f21ed9027f
edimetia3d <edimetia3d@gmail.com>
2021-08-29 01:09:12 +0800

    docs: add some doc for myself

--- srcs/lox/backend/virtual_machine/core/compiler.h
+++ srcs/lox/backend/virtual_machine/core/compiler.h
@@ -42,7 +42,23 @@ struct ParseRule {
 };
 
 /**
- * Compiler is basically a Pratt Parser, which could generate an IR that can work directly with stack machine.
+ * Compiler is mainly a Pratt Parser, which could generate an bytecode IR that can work directly with stack machine.
+ *
+ * There are some thing to note for myself.
+ * 1. In summary. Pratt Parser makes no essentially difference from RD parse, it is, just another parser,
+ * and all `emitXXX()` here could be replaced with a `return CreateXXXAstNode()` to build an AST.
+ * >> Read the http://journal.stuffwithstuff.com/2011/03/19/pratt-parsers-expression-parsing-made-easy/ will give you a smooth(maybe) transition from RD parser to
+ * Pratt Parser.
+ * 2. It's easy to see that, when we replace `CreateXXXAstNode()` to `emitXXX()` in parser,
+ * Obviously, the order of call to `emitXXX()` is same as `CreateXXXAstNode()`.
+ * 3. We can prove that, The order of call `EvaluateXXXAstNode()` could be implemented to be basically same as call `CreateXXXAstNode()` when evaluator walking in same AST.
+ * It's just that some control flow makes evaluation history changed a little. (If without control flow, the order would be exactly same.) So tree-walker evaluator
+ * is basically evaluate **all the node in their creation order**.
+ * This tells us: we could just follow the order of call `EvaluateXXXAstNode()` when running the emitted code to get correct answer.
+ *  a. To most emitted code, **first emitted code get run first too**
+ *  b. The `throw` we used in tree-walker could be simply simulated by a instruction pointer jump.
+ *  c. The return could be simulated by a stack, like we did in Visitor Pattern of AST evaluation. That's why our compiler work with
+ * stack machine naturally.
  */
 class Compiler {
  public:
