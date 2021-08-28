cadfb733b5f03e7c5ce4e24eb20b91a0ca80e41d
edimetia3d <edimetia3d@gmail.com>
2021-08-28 10:45:48 +0800

    add: vm compiler support simple expression

--- srcs/lox/backend/virtual_machine/core/compiler.cc
+++ srcs/lox/backend/virtual_machine/core/compiler.cc
@@ -4,6 +4,8 @@
 
 #include "lox/backend/virtual_machine/core/compiler.h"
 
+#include <map>
+
 namespace lox {
 
 namespace vm {
@@ -26,7 +28,7 @@ void DumpToken(Scanner &scanner) {
 ErrCode Compiler::Compile(Chunk *target) {
   current_trunk_ = target;
   Advance();
-  // Expression();
+  Expression();
   Consume(TokenType::EOF_TOKEN, "Expect end of expression.");
   endCompiler();
   if (parser_.hadError) {
@@ -39,7 +41,7 @@ void Compiler::Advance() {
 
   for (;;) {
     auto err = scanner_.ScanOne(&parser_.current);
-    if (err.NoError() && parser_.current->type != TokenType::EOF_TOKEN) break;
+    if (err.NoError()) break;
 
     errorAtCurrent(parser_.current->lexeme.c_str());
   }
@@ -73,5 +75,124 @@ Chunk *Compiler::CurrentChunk() { return current_trunk_; }
 void Compiler::endCompiler() { emitReturn(); }
 void Compiler::emitReturn() { emitOpCode(OpCode::OP_RETURN); }
 void Compiler::error(const char *message) { errorAt(parser_.previous, message); }
+void Compiler::Expression() { parsePrecedence(Precedence::PREC_ASSIGNMENT); }
+void Compiler::parsePrecedence(Precedence precedence) {
+  Advance();
+  auto prefixRule = getRule(parser_.previous->type)->prefix;
+  if (prefixRule == nullptr) {
+    error("Expect expression.");
+    return;
+  }
+
+  prefixRule(this);
+
+  while (precedence <= getRule(parser_.current->type)->precedence) {
+    Advance();
+    auto infixRule = getRule(parser_.previous->type)->infix;
+    infixRule(this);
+  }
+}
+ParseRule *Compiler::getRule(TokenType type) {
+#define RULE_ITEM(TOKEN_T, MEMBER_FN0, MEMBER_FN1, PRECEDENCE_V)                       \
+  {                                                                                    \
+    TokenType::TOKEN_T, ParseRule { MEMBER_FN0, MEMBER_FN1, Precedence::PRECEDENCE_V } \
+  }
+#define M(MEMBER_FN) &Compiler::MEMBER_FN
+  // clang-format off
+  static std::map<TokenType,ParseRule> rules    = {
+      RULE_ITEM(LEFT_PAREN   , M(grouping), nullptr  , PREC_NONE),
+      RULE_ITEM(RIGHT_PAREN  , nullptr    , nullptr  , PREC_NONE),
+      RULE_ITEM(LEFT_BRACE   , nullptr    , nullptr  , PREC_NONE),
+      RULE_ITEM(RIGHT_BRACE  , nullptr    , nullptr  , PREC_NONE),
+      RULE_ITEM(COMMA        , nullptr    , nullptr  , PREC_NONE),
+      RULE_ITEM(DOT          , nullptr    , nullptr  , PREC_NONE),
+      RULE_ITEM(MINUS        , M(unary)   , M(binary), PREC_TERM),
+      RULE_ITEM(PLUS         , nullptr    , M(binary), PREC_TERM),
+      RULE_ITEM(SEMICOLON    , nullptr    , nullptr  , PREC_NONE),
+      RULE_ITEM(SLASH        , nullptr    , M(binary), PREC_FACTOR),
+      RULE_ITEM(STAR         , nullptr    , M(binary), PREC_FACTOR),
+      RULE_ITEM(BANG         , nullptr    , nullptr  , PREC_NONE),
+      RULE_ITEM(BANG_EQUAL   , nullptr    , nullptr  , PREC_NONE),
+      RULE_ITEM(EQUAL        , nullptr    , nullptr  , PREC_NONE),
+      RULE_ITEM(EQUAL_EQUAL  , nullptr    , nullptr  , PREC_NONE),
+      RULE_ITEM(GREATER      , nullptr    , nullptr  , PREC_NONE),
+      RULE_ITEM(GREATER_EQUAL, nullptr    , nullptr  , PREC_NONE),
+      RULE_ITEM(LESS         , nullptr    , nullptr  , PREC_NONE),
+      RULE_ITEM(LESS_EQUAL   , nullptr    , nullptr  , PREC_NONE),
+      RULE_ITEM(IDENTIFIER   , nullptr    , nullptr  , PREC_NONE),
+      RULE_ITEM(STRING       , nullptr    , nullptr  , PREC_NONE),
+      RULE_ITEM(NUMBER       , M(number)  , nullptr  , PREC_NONE),
+      RULE_ITEM(AND          , nullptr    , nullptr  , PREC_NONE),
+      RULE_ITEM(CLASS        , nullptr    , nullptr  , PREC_NONE),
+      RULE_ITEM(ELSE         , nullptr    , nullptr  , PREC_NONE),
+      RULE_ITEM(FALSE        , nullptr    , nullptr  , PREC_NONE),
+      RULE_ITEM(FOR          , nullptr    , nullptr  , PREC_NONE),
+      RULE_ITEM(FUN          , nullptr    , nullptr  , PREC_NONE),
+      RULE_ITEM(IF           , nullptr    , nullptr  , PREC_NONE),
+      RULE_ITEM(NIL          , nullptr    , nullptr  , PREC_NONE),
+      RULE_ITEM(OR           , nullptr    , nullptr  , PREC_NONE),
+      RULE_ITEM(PRINT        , nullptr    , nullptr  , PREC_NONE),
+      RULE_ITEM(RETURN       , nullptr    , nullptr  , PREC_NONE),
+      RULE_ITEM(THIS         , nullptr    , nullptr  , PREC_NONE),
+      RULE_ITEM(TRUE         , nullptr    , nullptr  , PREC_NONE),
+      RULE_ITEM(VAR          , nullptr    , nullptr  , PREC_NONE),
+      RULE_ITEM(WHILE        , nullptr    , nullptr  , PREC_NONE),
+      RULE_ITEM(EOF_TOKEN    , nullptr    , nullptr  , PREC_NONE),
+  };
+  // clang-format on
+#undef RULE_ITEM
+#undef M
+  return &rules[type];
+}
+void Compiler::emitBytes(OpCode byte1, uint8_t byte2) {
+  emitOpCode(byte1);
+  emitByte(byte2);
+}
+uint8_t Compiler::makeConstant(Value value) {
+  int constant = CurrentChunk()->addConstant(value);
+  if (constant > UINT8_MAX) {
+    error("Too many constants in one chunk.");
+    return 0;
+  }
+
+  return (uint8_t)constant;
+}
+void Compiler::unary() {
+  TokenType operatorType = parser_.previous->type;
+
+  // Compile the operand.
+  parsePrecedence(Precedence::PREC_UNARY);
+
+  // Emit the operator instruction.
+  switch (operatorType) {
+    case TokenType::MINUS:
+      emitOpCode(OpCode::OP_NEGATE);
+      break;
+    default:
+      return;  // Unreachable.
+  }
+}
+void Compiler::binary() {
+  TokenType operatorType = parser_.previous->type;
+  ParseRule *rule = getRule(operatorType);
+  parsePrecedence((Precedence)((int)(rule->precedence) + 1));
+
+  switch (operatorType) {
+    case TokenType::PLUS:
+      emitOpCode(OpCode::OP_ADD);
+      break;
+    case TokenType::MINUS:
+      emitOpCode(OpCode::OP_SUBTRACT);
+      break;
+    case TokenType::STAR:
+      emitOpCode(OpCode::OP_MULTIPLY);
+      break;
+    case TokenType::SLASH:
+      emitOpCode(OpCode::OP_DIVIDE);
+      break;
+    default:
+      return;  // Unreachable.
+  }
+}
 }  // namespace vm
 }  // namespace lox
--- srcs/lox/backend/virtual_machine/core/compiler.h
+++ srcs/lox/backend/virtual_machine/core/compiler.h
@@ -5,6 +5,8 @@
 #ifndef LOX_SRCS_LOX_BACKEND_VIRTUAL_MACHINE_CORE_COMPILER_H_
 #define LOX_SRCS_LOX_BACKEND_VIRTUAL_MACHINE_CORE_COMPILER_H_
 
+#include <functional>
+
 #include "lox/backend/virtual_machine/bytecode/chunk.h"
 #include "lox/backend/virtual_machine/common/err_code.h"
 #include "lox/frontend/scanner.h"
@@ -17,6 +19,28 @@ struct Parser {
   bool hadError = false;
   bool panicMode = false;
 };
+
+enum class Precedence {
+  PREC_NONE,
+  PREC_ASSIGNMENT,  // =
+  PREC_OR,          // or
+  PREC_AND,         // and
+  PREC_EQUALITY,    // == !=
+  PREC_COMPARISON,  // < > <= >=
+  PREC_TERM,        // + -
+  PREC_FACTOR,      // * /
+  PREC_UNARY,       // ! -
+  PREC_CALL,        // . ()
+  PREC_PRIMARY
+};
+
+class Compiler;
+struct ParseRule {
+  std::function<void(Compiler*)> prefix;
+  std::function<void(Compiler*)> infix;
+  Precedence precedence;
+};
+
 class Compiler {
  public:
   Compiler(Scanner& scanner) : scanner_(scanner) {}
@@ -29,10 +53,7 @@ class Compiler {
   void errorAt(Token token, const char* message);
   void Consume(TokenType type, const char* message);
   void emitByte(uint8_t byte) { CurrentChunk()->WriteUInt8(byte, parser_.previous->line); }
-  void emitBytes(uint8_t byte1, uint8_t byte2) {
-    emitByte(byte1);
-    emitByte(byte2);
-  }
+  void emitBytes(OpCode byte1, uint8_t byte2);
   void emitOpCode(OpCode opcode) { emitByte(static_cast<uint8_t>(opcode)); }
   Chunk* CurrentChunk();
   Parser parser_;
@@ -40,7 +61,23 @@ class Compiler {
   Chunk* current_trunk_;
   void endCompiler();
   void emitReturn();
+  uint8_t makeConstant(Value value);
+  void emitConstant(Value value) { emitBytes(OpCode::OP_CONSTANT, makeConstant(value)); }
+  void number() {
+    double value = std::stod(parser_.previous->lexeme);
+    emitConstant(value);
+  }
+  void Expression();
+  void grouping() {
+    Expression();
+    Consume(TokenType::LEFT_PAREN, "Expect ')' after expression.");
+  }
+  void unary();
+  void parsePrecedence(Precedence precedence);
+  ParseRule* getRule(TokenType type);
+  void binary();
 };
+
 }  // namespace vm
 }  // namespace lox
 #endif  // LOX_SRCS_LOX_BACKEND_VIRTUAL_MACHINE_CORE_COMPILER_H_
