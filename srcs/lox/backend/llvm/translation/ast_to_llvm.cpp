5f7e05b1eb44ad0d17ab385f1fadc97307924324
edimetia3d <edimetia3d@gmail.com>
2022-05-27 09:58:01 +0800

    feat(wip): update llvm backend symbol reslove

--- srcs/lox/backend/llvm/translation/ast_to_llvm.cpp
+++ srcs/lox/backend/llvm/translation/ast_to_llvm.cpp
@@ -11,13 +11,13 @@ namespace lox::llvm_jit {
 
 struct BasicBlockDef {
   // Maps the variable (or formal parameter) to its definition.
-  llvm::DenseMap<llvm::StringRef, llvm::TrackingVH<llvm::Value>> Defs;
+  llvm::DenseMap<llvm::StringRef, llvm::TrackingVH<llvm::Value>> defs;
   // Set of incompleted phi instructions.
-  llvm::DenseMap<llvm::PHINode *, llvm::StringRef> IncompletePhis;
-  // Block is sealed, that is, no more predecessors will be added.
-  unsigned Sealed : 1;
+  llvm::DenseMap<llvm::PHINode *, llvm::StringRef> incomplete_phis;
+  // Block is sealed, that is, all predecessors of the bb are added.
+  bool sealed;
 
-  BasicBlockDef() : Sealed(0) {}
+  BasicBlockDef() : sealed(false) {}
 };
 
 class ASTToLLVM : public lox::ASTNodeVisitor<llvm::Value *> {
@@ -82,6 +82,8 @@ class ASTToLLVM : public lox::ASTNodeVisitor<llvm::Value *> {
     SwitchBB(while_body_bb);
     NoValueVisit(node->body);
     builder_.CreateBr(while_cond_bb);
+    sealBlock(while_body_bb);
+    sealBlock(while_cond_bb);
 
     // switch back
     SwitchBB(after_while_bb);
@@ -136,7 +138,71 @@ class ASTToLLVM : public lox::ASTNodeVisitor<llvm::Value *> {
  protected:
   llvm::Type *double_ty_;
 
-  llvm::DenseMap<llvm::BasicBlock *, BasicBlockDef> CurrentDef;
+  llvm::DenseMap<llvm::BasicBlock *, BasicBlockDef> current_defs_;
+
+  void write_local_variable(llvm::BasicBlock *BB, llvm::StringRef var_name, llvm::Value *value) {
+    current_defs_[BB].defs[var_name] = value;
+  }
+  llvm::Value *readLocalVariable(llvm::BasicBlock *BB, llvm::StringRef var_name) {
+    auto Val = current_defs_[BB].defs.find(var_name);
+    if (Val != current_defs_[BB].defs.end()) return Val->second;
+    // when code goes here, a variable could only be found in other bb, so a phi will be needed.
+    return read_var_from_predecessor(BB, var_name);
+  }
+  llvm::Value *read_var_from_predecessor(llvm::BasicBlock *BB, llvm::StringRef var_name) {
+    llvm::Value *Val = nullptr;
+    if (!current_defs_[BB].sealed) {
+      // it is possible that not all predecessor is known
+      llvm::PHINode *Phi = CreateEmptyPhi(BB, var_name);
+      current_defs_[BB].incomplete_phis[Phi] = var_name;
+      Val = Phi;
+    } else if (auto *PredBB = BB->getSinglePredecessor()) {
+      Val = readLocalVariable(PredBB, var_name);
+    } else {
+      llvm::PHINode *Phi = CreateEmptyPhi(BB, var_name);
+      Val = Phi;
+      write_local_variable(BB, var_name, Val);
+      addPhiOperands(BB, var_name, Phi);
+    }
+    write_local_variable(BB, var_name, Val);
+    return Val;
+  }
+
+  void addPhiOperands(llvm::BasicBlock *BB, llvm::StringRef var_name, llvm::PHINode *Phi) {
+    for (auto I = llvm::pred_begin(BB), E = llvm::pred_end(BB); I != E; ++I) {
+      Phi->addIncoming(readLocalVariable(*I, var_name), *I);
+    }
+    optimizePhi(Phi);
+  }
+
+  void optimizePhi(llvm::PHINode *Phi) {
+    llvm::Value *Same = nullptr;
+    for (llvm::Value *V : Phi->incoming_values()) {
+      if (V == Same || V == Phi) continue;
+      if (Same && V != Same) return;
+      Same = V;
+    }
+    if (Same == nullptr) Same = llvm::UndefValue::get(Phi->getType());
+    // Collect phi instructions using this one.
+    llvm::SmallVector<llvm::PHINode *, 8> CandidatePhis;
+    for (llvm::Use &U : Phi->uses()) {
+      if (auto *P = llvm::dyn_cast<llvm::PHINode>(U.getUser()))
+        if (P != Phi) CandidatePhis.push_back(P);
+    }
+    Phi->replaceAllUsesWith(Same);
+    Phi->eraseFromParent();
+    for (auto *P : CandidatePhis) optimizePhi(P);
+  }
+
+  llvm::PHINode *CreateEmptyPhi(llvm::BasicBlock *BB, llvm::StringRef var_name) {
+    return BB->empty() ? llvm::PHINode::Create(mapType(var_name), 0, "", BB)
+                       : llvm::PHINode::Create(mapType(var_name), 0, "", &BB->front());
+  }
+
+  llvm::Type *mapType(llvm::StringRef var_name) {
+    // todo: this function should get the type of var_name
+    return double_ty_;
+  }
 
   llvm::LLVMContext &context_;
   std::unique_ptr<llvm::Module> ll_module_;
@@ -146,6 +212,15 @@ class ASTToLLVM : public lox::ASTNodeVisitor<llvm::Value *> {
   void SwitchBB(llvm::BasicBlock *bb) {
     active_bb_ = bb;
     builder_.SetInsertPoint(active_bb_);
+  }
+  void sealBlock(llvm::BasicBlock *BB) {
+    // a seal should usually be called before some switchBB
+    assert(!current_defs_[BB].sealed && "Attempt to seal already sealed block");
+    for (auto PhiDecl : current_defs_[BB].incomplete_phis) {
+      addPhiOperands(BB, PhiDecl.second, PhiDecl.first);
+    }
+    current_defs_[BB].incomplete_phis.clear();
+    current_defs_[BB].sealed = true;
   }
 };
 
